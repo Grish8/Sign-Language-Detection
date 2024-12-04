5bb0ed74d1fccad901feef5e9c734372a0be98fd
Grish8 <grishamenos@gmail.com>
2024-12-04 20:48:46 +0300

    testing classifier

--- inference_classifier.py
+++ inference_classifier.py
@@ -1,10 +1,11 @@
+#testing classifier
 import pickle
 
 import cv2
 import mediapipe as mp
 import numpy as np
 
-model_dict = pickle.load(open('./model.p', 'rb'))
+model_dict = pickle.load(open('model.p', 'rb'))
 model = model_dict['model']
 
 cap = cv2.VideoCapture(2)
